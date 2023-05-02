34f43ddc95c7ddb04827f4423da419511a7a3fdb
Sunilkushwah7289 <knowledgenetwork18@gmail.com>
2023-05-02 15:17:14 +0530

    Sample commit

---
+++ web_app/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:web_app/webview.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: WebView(),
+  ));
+}
---
+++ web_app/lib/webview.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebView extends StatefulWidget {
+  const WebView({super.key});
+
+  @override
+  State<WebView> createState() => _WebViewState();
+}
+
+class _WebViewState extends State<WebView> {
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://youtube.com'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Youtube Web Clone',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.red,
+        elevation: 0,
+      ),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
